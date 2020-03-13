55b2efcac2661d94fa895d98abb6e6f2422846fa
ahmedakef <aemed.akef.1@gmail.com>
2020-03-13 16:25:12 +0200

    [dlocate] solve metadate for linux

--- dlocate/osutils/utils_linux.go
+++ dlocate/osutils/utils_linux.go
@@ -3,14 +3,20 @@ package osutils
 import (
 	"os"
 	"syscall"
+	"time"
 )
 
 //GetFileInfo return file metadata sturct using it's path
 func GetFileInfo(fileinfo os.FileInfo, path string) FileMetadata {
-	var file FileMetadata = NewFileMetadata(fileinfo)
-	file.Path = path + "\\" + file.Name
-	file.ATime = fileinfo.Sys().(*syscall.Stat_t).Atim
-	file.CTime = fileinfo.Sys().(*syscall.Stat_t).Ctim
-	file.MTime = fileinfo.Sys().(*syscall.Stat_t).Mtim
+	var file = NewFileMetadata(fileinfo)
+	file.Path = path + "/" + file.Name
+	aTime := fileinfo.Sys().(*syscall.Stat_t).Atim
+	cTime := fileinfo.Sys().(*syscall.Stat_t).Ctim
+	mTime := fileinfo.Sys().(*syscall.Stat_t).Mtim
+
+	file.ATime = time.Unix(aTime.Sec, aTime.Nsec)
+	file.CTime = time.Unix(cTime.Sec, cTime.Nsec)
+	file.MTime = time.Unix(mTime.Sec, mTime.Nsec)
+
 	return file
 }
